3a16afc0789a5111e022eda27ec1e02e440a8d0d
Tulip <93089383+Tulip-0333@users.noreply.github.com>
2022-07-03 22:02:33 -0400

    New commands!

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 const Discord = require('discord.js');
-//Make sure to remove this when you upload this to Github!
-const token = "Your bot's token"
+//LEL you thought you could steal my token :))))
+const token = "TOKEN!";
 
 const client = new Discord.Client({
     allowedMentions: {
         parse: ['users', 'roles'],
-        repliiedUser: true
+        repliedUser: true
     },
     intents: [
         "GUILDS",
@@ -28,15 +28,34 @@ client.on('messageCreate', message => {
     if (message.content === "*help") {
         const helpembed = new Discord.MessageEmbed()
         .setTitle("Help command")
-        .setDescription("Commands: *help*, roulette, ping")
+        .setDescription("Commands: *help*, roulette, ping, invite, info")
         .setFooter("Prefix is asterisk. Do *help (command) for help on a specific command.");
         message.channel.send({embeds:[helpembed]});
+    }
+    if (message.content === "*debug1") {
+        const advancedhelpembed = new Discord.MessageEmbed()
+        .setTitle("Debug")
+        .setDescription("You shouldn't know about this...");
+        message.channel.send(advancedhelpembed);
+        message.channel.send("`"+FileList+ "`");
+    }
+    if (message.content === "*help info") {
+        const helpinfoembed = new Discord.MessageEmbed()
+        .setTitle("Help with info")
+        .setDescription("Info gives lots of info")
+        message.channel.send({embeds:[helpinfoembed]});
     }
     if (message.content === "*help roulette") {
         const roulettehelpembed = new Discord.MessageEmbed()
         .setTitle("Help with roulette")
         .setDescription("Roulette has a 1/2 chance for you to win.");
         message.channel.send({embeds:[roulettehelpembed]});
+    }
+    if (message.content === "*help invite") {
+        const helpinviteembed = new Discord.MessageEmbed()
+        .setTitle("Help with invite")
+        .setDescription("Gives a link to invite the bot.")
+        message.channel.send({embeds:[helpinfoembed]});
     }
     if (message.content === "*help ping") {
         const pinghelpembed = new Discord.MessageEmbed()
@@ -49,8 +68,30 @@ client.on('messageCreate', message => {
         message.channel.send(rouletteR)
     }
     if (message.content === "*ping") {
-        console.log(message.author.toString());
-        message.reply(message.author.toString() + " pong!");
+        let factR = facts[Math.floor(Math.random() * facts.length)];
+        const pingembed = new Discord.MessageEmbed()
+        .setTitle("pong!")
+        .setDescription(factR);
+        message.channel.send({embeds:[pingembed]});
+    }
+    if (message.content === "*invite") {
+        const inviteembed = new Discord.MessageEmbed()
+        .setTitle("Invite me here!:")
+        .setDescription("https://discord.com/oauth2/authorize?client_id=984249425591431188&scope=bot&permissions=8");
+        message.channel.send({embeds:[inviteembed]});
+    }
+    if (message.content === "*info") {
+        const infoembed = new Discord.MessageEmbed()
+        .setTitle("Info")
+        .setDescription("Tulip's Fancy Assistant is a Discord bot made by Tulip#0333. If you find any bugs, go to my Github profile (Tulip-0333). Tell me any features you want added, if there are bugs, tell me. More facts: ping command.");
+        message.channel.send({embeds:[infoembed]});
+    }
+    if (message.content === "*whois") {
+        const whoisembed = new Discord.MessageEmbed()
+        .setTitle(message.author.username+"'s info")
+        .setImage(message.author.avatar)
+        .setDescription("ID: "+message.author.id);
+        message.channel.send({embeds:[whoisembed]});
     }
 })
 
